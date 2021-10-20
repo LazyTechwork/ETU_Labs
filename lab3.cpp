8e56962b5692a48fa5ac51270150a092e60a3bcf
LazyTechwork <lazytechwork@gmail.com>
2021-10-20 22:08:39 +0300

    Improvements in solution of lab3

--- lab3.cpp
+++ lab3.cpp
@@ -4,6 +4,20 @@ enum Direction {
     LEFT, RIGHT, UP, DOWN
 };
 
+void square_matrix(const int n, int **matrix) {
+    for (int i = 0; i < n; ++i)
+        for (int j = 0; j < n; ++j)
+            *(*(matrix + i) + j) *= *(*(matrix + i) + j);
+}
+
+void print_matrix(const int n, int **matrix) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j)
+            std::cout << *(*(matrix + i) + j) << "\t";
+        std::cout << std::endl;
+    }
+}
+
 int lab3() {
     int n;
     std::cin >> n;
@@ -11,7 +25,7 @@ int lab3() {
     Direction direction = Direction::RIGHT;
     Direction previousDirection;
     for (int i = 1, l = n * n, x = 0, y = n - 1, xCorner = y, yCorner = y - 1; i <= l; ++i) {
-        *(*(a + y) + x) = i*i;
+        *(*(a + y) + x) = i;
         switch (direction) {
             case Direction::RIGHT:
                 if (x != xCorner) {
@@ -67,11 +81,16 @@ int lab3() {
                 break;
         }
     }
-    for (auto &i: a) {
-        for (int element: i)
-            std::cout << element << "\t";
-        std::cout << std::endl;
-    }
+
+//    Convert 2D array to pointer matrix
+    int *matrix[n];
+    for (int i = 0; i < n; ++i)
+        *(matrix + i) = *(a + i);
+
+    print_matrix(n, matrix);
+    square_matrix(n, matrix);
+    std::cout << std::string(1 << 6, '=') << std::endl;
+    print_matrix(n, matrix);
 
     return 0;
 }
